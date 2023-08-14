6ac038d7b26b5a0cd8f5f17359ea0b93b44156df
Junaid Qadir Shekhanzai <shekhanzai.baloch@gmail.com>
2023-08-15 00:56:35 +0500

    Add jq/cray

---
+++ config/cray.php
@@ -0,0 +1,58 @@
+<?php
+
+return [
+    'stubs_dir' => resource_path('stubs'),
+    /**
+     * When generating views, the views will be generated in the directory specified by the views_template key.
+     * The views can be found in [stubs_dir]/views.
+     */
+    'views_template' => 'tailwind',
+
+    'fields' => [
+        /**
+         * When enabled, when running cray ModelName will generate form fields from its migration.
+         */
+        'generate' => true,
+
+        /**
+         * Component paths for each field type.
+         * For example, `components.text` looks for a file in resources\views\components\text.blade.php.
+         */
+        'component_paths' => [
+            'input_text' => 'components.text',
+            'input_number' => 'components.number',
+            'input_checkbox' => 'components.checkbox',
+            'input_radio' => 'components.radio',
+            'input_file' => 'components.file',
+            'input_date' => 'components.text',
+            'textarea' => 'components.textarea',
+        ],
+
+        /**
+         * Fields under the ignore_fields key will not be generated.
+         */
+        'ignore' => [
+            'id',
+            'created_at',
+            'updated_at',
+            'deleted_at',
+        ],
+
+        'localization' => [
+            /**
+             * When enabled, labels will be generated with __() method.
+             */
+            'enabled' => false,
+
+            /**
+             * /resources/lang/en/messages.php.
+             */
+            'key_container' => 'messages',
+
+            /**
+             * When true, the evaluated value returned from the __() function will be used.
+             */
+            'render' => false,
+        ],
+    ],
+];
